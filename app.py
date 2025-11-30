3a7a8f16e9be99c3804a2e11ff09b90d7be00673
olha-d-d-00 <olgadema39@gmail.com>
2025-12-01 01:35:47 +0200

    HomeWork_4

--- app.py
+++ app.py
@@ -1,17 +1,86 @@
 from flask import Flask, render_template
+from flask import request
 import sqlite3
 app = Flask(__name__)
 
+def film_dictionary(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+class db_connection:
+    def __init__(self):
+        self.conn = sqlite3.connect('database.db')
+        self.conn.row_factory = film_dictionary
+        self.cur = self.conn.cursor()
+
+    def __enter__(self):
+        return self.cur
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.commit()
+        self.conn.close()
+
+
+def get_db_result(query):
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = film_dictionary
+    cur = conn.cursor()
+    res = cur.execute(query)
+    result = res.fetchall()
+    conn.close()
+    return result
+
+
 @app.route('/')
 def main_page():  # put application's code here
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    res = cur.execute('SELECT id, poster, name FROM film order by added_at desc limit 10')
-    result = res.fetchall()
+    with db_connection() as cur:
+       result = cur.execute("SELECT * FROM film order by added_at desc limit 10").fetchall()
+
+    # result = get_db_result('SELECT id, poster, name FROM film order by added_at desc limit 10')
     return result
+
+@app.route('/register', methods=['GET'])
+def register_page():
+    return """
+    <form action="/register" method="post">
+    
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname"><br>
+    
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname"><br>
+    
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password"><br>
+    
+    <label for="login">Login:</label><br>
+    <input type="text" id="login" name="login"><br>
+    
+    <label for="email">Email:</label><br>
+    <input type="email" id="email" name="email"><br>
+    
+    <label for="birth_day">Birth date:</label><br>
+    <input type="date" id="birth_day" name="birth_date"><br>
+    
+    
+    <input type="submit" value="Submit">
+    </form>
+    """
+
 
 @app.route('/register', methods=['POST'])
 def user_register():
+    first_name = request.form['fname']
+    last_name = request.form['lname']
+    password = request.form['password']
+    login = request.form['login']
+    email = request.form['email']
+    birth_date = request.form['birth_date']
+    with db_connection() as cur:
+        cur.execute('INSERT INTO user (first_name, last_name, password, login, email, birth_date) VALUES (?, ?, ?, ?, ?, ?)',
+                    (first_name, last_name, password, login, email, birth_date))
     return 'Register'
 
 @app.post('/login')
@@ -32,10 +101,7 @@ def user_delete(user_id):
 
 @app.route('/films', methods=['GET'])
 def films():
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    res = cur.execute('SELECT id, poster, name FROM film order by added_at desc')
-    result = res.fetchall()
+    result = get_db_result('SELECT id, poster, name FROM film order by added_at desc')
     return result
 
 @app.route('/films', methods=['POST'])
@@ -44,13 +110,15 @@ def film_add():
 
 @app.route('/films/<film_id>', methods=['GET'])
 def film_info(film_id):
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    res = cur.execute(f"SELECT * FROM film where id={film_id}")
-    result = res.fetchone()
+    with db_connection() as cur:
+        result = cur.execute(f"SELECT * FROM film where id={film_id}").fetchall()
+        actors = cur.execute(f"SELECT * FROM actor join actor_film on actor.id == actor_film.actor_id where actor_film.film_id={film_id}").fetchall()
+        genres = get_db_result(f"SELECT * FROM genre_film where film_id={film_id}").fetchall()
 
-    actors = cur.execute(f"SELECT * FROM actor join actor_film on actor.id == actor_film.actor_id where actor_film.film_id={film_id}").fetchall()
-    genres = cur.execute(f"SELECT * FROM genre_film where film_id={film_id}").fetchall()
+
+    # result = get_db_result(f"SELECT * FROM film where id={film_id}")
+    # actors = get_db_result(f"SELECT * FROM actor join actor_film on actor.id == actor_film.actor_id where actor_film.film_id={film_id}")
+    # genres = get_db_result(f"SELECT * FROM genre_film where film_id={film_id}")
     return f'Film {film_id} is {result}, actors: {actors}, genres: {genres}'
 
 @app.route('/films/<film_id>', methods=['PUT'])
@@ -109,25 +177,24 @@ def user_list_item_delete(user_id, list_id, film_id):
     return f'User {user_id} list item {list_id} deleted'
 
 @app.get('/login')
-def login_page():
+def login_pages():
     return render_template('login.html')
 
-app.post('/login')
+@app.post('/login')
 def users_login():
     return 'Login'
 
-app.get('/logout')
+@app.get('/logout')
 def users_logout():
     return 'Logout'
 
-@app.get('/register')
-def register_page():
+@app.get('/registration')
+def register_pages():
     return render_template('register.html')
 
 @app.post('/register')
 def users_register():
     return 'Register'
-
 
 
 if __name__ == '__main__':
